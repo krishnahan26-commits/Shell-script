6c6b0b73423e23706d50d3c20185aad71f5726c2
Krishnahan26 <Krishahan26@gmail.com>
2025-12-23 00:30:21 +0530

    Good changes

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,10 +4,7 @@ echo "Please enter your username :"
 
 read -s USERNAME
 
-echo "Username enterd : $USERNAME
+echo "Username enterd : $USERNAME"
 
-echo "Please enter your password :"
-
-read -s PASSWORD
 
 
