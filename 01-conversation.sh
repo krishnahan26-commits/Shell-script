f7f5780f2f2ede7389f9d85fd18a02cbf23fb9bb
Krishnahan26 <Krishahan26@gmail.com>
2025-12-22 23:46:36 +0530

    Good changes

--- 01-conversation.sh
+++ 01-conversation.sh
@@ -3,7 +3,7 @@
 PERSON1=Rohith
 PERSON2=Virat
 
-echo "PERSON1: Hi"
-echo "PERSON2: Hi mama"
-echo "PERSON1: yela unnav mawa"
-echo "PERSON2: fine mawa"
+echo "$PERSON1: Hi"
+echo "$PERSON2: Hi mama"
+echo "$PERSON1: yela unnav mawa"
+echo "$PERSON2: fine mawa"
