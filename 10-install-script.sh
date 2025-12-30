26294752e22da684920a9a115d9cc7a82756a22f
Krishnahan26 <Krishahan26@gmail.com>
2025-12-30 16:44:29 +0530

    Installatio script

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -7,23 +7,53 @@ then
     echo "Error : you must have sudo access to execute this script"
     exit 1
 fi
-
-dnf install mysql -y
+dnf list isntalled mysql
 
 if [ $? -ne 0 ]
 then
-    echo "Installing MySQL.... FAILURE"
-    exit 1
-else
-    echo "Installing MySQL.... SUCCESS"
+    dnf install mysql -y
+
+    if [ $? -ne 0 ]
+    then
+        echo " Intalling MySQL....FAILURE"
+        exit 1
+    else
+        echo "Installing MySQL....SUCCESS"
+    fi
+    else
+        echo "MySql is already.....INSTALLED"
+    fi
+
+
+# if [ $? -ne 0 ]
+# then
+#     echo "Installing MySQL.... FAILURE"
+#     exit 1
+# else
+#     echo "Installing MySQL.... SUCCESS"
+# fi
+dnf list intalled git
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+
+    if[ $? -ne 0 ]
+    then
+        echo "Installing git....FAILURE"
+        exit 1
+    else
+        echo "Installing git.....SUCCESS"
 fi
 
-dnf install git -y
-
-if [ $? -ne 0 ]
-then
-    echo "Installing git.....FAILURE"
-    exit 1
-else
-    echo "Installing git.....SUCCESS"
+    else
+        echo "Git is already INSTALLED"
     fi
+
+# if [ $? -ne 0 ]
+# then
+#     echo "Installing git.....FAILURE"
+#     exit 1
+# else
+#     echo "Installing git.....SUCCESS"
+#     fi
