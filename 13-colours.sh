d9a29aff52ba40a220b611efb670bc435b7baafa
Krishnahan26 <Krishahan26@gmail.com>
2026-01-05 22:43:31 +0530

    Adding colours for good user experiance

---
+++ 13-colours.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ....$R FAILURE"
+        exit 1
+    else
+        echo -e "$2 .... $G SUCCESS"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR : YOU MUST HAVE SUDO ACCESS TO EXECUTE THIS SCRIPT"
+    exit 1
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y
+    VALIDATE $? "Installing MySQL"
+else
+    echo -e "MySQL is already $Y installed"
+fi
+
+dnf list intalled git
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    VALIDATE $? "Installing git"
+else
+    echo "Git is already $Y installed"
+fi
+
