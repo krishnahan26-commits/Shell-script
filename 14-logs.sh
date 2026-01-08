d40e7566da9522419030dcccc9e6e04989d18508
Krishnahan26 <Krishahan26@gmail.com>
2026-01-09 01:16:21 +0530

    log file created

--- 14-logs.sh
+++ 14-logs.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
 LOGS_FOLDER="/var/log/shell-script-logs"
 LOG_FILE=$( echo $0 | cut -d "." -f1 )
 TIMESTAMP=$( date +%Y-%m-%d-%H-%M-%S )
@@ -11,10 +8,10 @@ LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE/$TIMESTAMP.log"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 ....$R FAILURE"
+        echo -e "$2 .... FAILURE"
         exit 1
     else
-        echo -e "$2 .... $G SUCCESS"
+        echo -e "$2 ....  SUCCESS"
     fi
 }
 
