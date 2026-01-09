3e8371438f71884cbf15dae6e4366909ccab1f1d
Krishnahan26 <Krishahan26@gmail.com>
2026-01-09 23:35:28 +0530

    Log script

--- 14-logs.sh
+++ 14-logs.sh
@@ -8,6 +8,7 @@ LOGS_FOLDER="/var/log/shell-script-logs"
 LOG_FILE=$( echo $0 | cut -d "." -f1 )
 TIMESTAMP=$( date +%Y-%m-%d-%H-%M-%S )
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE/$TIMESTAMP.log"
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
